ed4941e6197489fdea62909aba75b2c9c91ae642
Neelesh Ranjan <neeleshranjan95@gmail.com>
2023-02-23 14:24:30 +0530

    Nex ChatGPT Frontend

--- src/Components/ChatGPT/ChatGPT.js
+++ src/Components/ChatGPT/ChatGPT.js
@@ -24,7 +24,7 @@ const ChatGPT = (props) => {
 
   const configuration = new Configuration({
     organization: "org-L2GZ4kXnhDM2W2TwVicIEJHd",
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
 
